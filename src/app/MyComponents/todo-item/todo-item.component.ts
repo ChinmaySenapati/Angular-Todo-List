1a4d6628066a934023c8232402caed0901be4a96
chinmaysenapati <chinmaysenapati07@gmail.com>
2023-02-23 11:56:12 +0530

    second commit

--- src/app/MyComponents/todo-item/todo-item.component.ts
+++ src/app/MyComponents/todo-item/todo-item.component.ts
@@ -8,9 +8,15 @@ import { Todo } from 'src/app/Todo';
 })
 export class TodoItemComponent {
   @Input() todo!: Todo;
+  @Input() i!: number;
   @Output() todoDelete: EventEmitter<Todo> = new EventEmitter();
+  @Output() todoCheckBox: EventEmitter<Todo> = new EventEmitter();
   onClick(todo: Todo) {
     this.todoDelete.emit(todo);
     console.log('onclick triggered');
+  }
+
+  onCheckboxClick(todo: Todo) {
+    this.todoCheckBox.emit(todo);
   }
 }
--- src/app/MyComponents/todos/todos.component.ts
+++ src/app/MyComponents/todos/todos.component.ts
@@ -7,38 +7,54 @@ import { Todo } from 'src/app/Todo';
   styleUrls: ['./todos.component.css'],
 })
 export class TodosComponent {
+  localItem;
   todos!: Todo[];
+
   constructor() {
-    this.todos = [
-      {
-        sno: 1,
-        title: 'This is title1',
-        desc: 'Description1',
-        active: true,
-      },
-      {
-        sno: 2,
-        title: 'This is title2',
-        desc: 'Description2',
-        active: true,
-      },
-      {
-        sno: 3,
-        title: 'This is title3',
-        desc: 'Description3',
-        active: true,
-      },
-    ];
+    this.localItem = localStorage.getItem('todos');
+    if (this.localItem == null) {
+      this.todos = [
+        // {
+        //   sno: 1,
+        //   title: 'This is title1',
+        //   desc: 'Description1',
+        //   active: true,
+        // },
+        // {
+        //   sno: 2,
+        //   title: 'This is title2',
+        //   desc: 'Description2',
+        //   active: true,
+        // },
+        // {
+        //   sno: 3,
+        //   title: 'This is title3',
+        //   desc: 'Description3',
+        //   active: true,
+        // },
+      ];
+    } else {
+      // console.log(this.todos);
+      this.todos = JSON.parse(this.localItem);
+    }
   }
 
   deleteTodo(todo: Todo) {
     console.log(todo);
     const index = this.todos.indexOf(todo);
     this.todos.splice(index, 1);
+    localStorage.setItem('todos', JSON.stringify(this.todos));
   }
 
   addTodo(todo: Todo) {
     console.log(todo);
     this.todos.push(todo);
+    localStorage.setItem('todos', JSON.stringify(this.todos));
+  }
+
+  toggleTodo(todo: Todo) {
+    const index = this.todos.indexOf(todo);
+    this.todos[index].active = !this.todos[index].active;
+    localStorage.setItem('todos', JSON.stringify(this.todos));
   }
 }
